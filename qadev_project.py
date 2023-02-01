dfafd7ca8f90950bfe45403d48bbb5a0d2ea9be4
czeslaw187 <czeslaw187@gmail.com>
2023-02-01 10:09:09 +0000

    find user or insert new

--- qadev_project.py
+++ qadev_project.py
@@ -1,42 +1,43 @@
-# import mysql.connector
-#
-# mydb = mysql.connector.connect(
-#       host="localhost",
-#       user="yourusername",
-#       password="yourpassword"
-# )
-#
-# print(mydb)
+import mysql.connector
+
+mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="Pa$$w0rd",
+      database="qastore"
+)
+
+client = mydb.cursor()
 
 users = [
       {'name': 'Tedd Bundy', 'score': 50},
       {'name': 'Homer Simpson', 'score': 75},
       {'name': 'John Doe', 'score': 68}
 ]
+
+def add_new_user(name):
+      print('Add new user \n')
+      ict_score = input('Enter new ICT score: ')
+      math_score = input("Enter new Math score: ")
+      chemistry_score = input("Enter new chemistry score: ")
+      client.execute("INSERT INTO students (name, ict, math, chemistry) VALUES (%s, %s, %s, %s)",(name, ict_score, math_score, chemistry_score))
+      mydb.commit()
+      if client.rowcount == True:
+            print("Record inserted")
+
 # find user
 
+print('Find user')
+fname = input('Enter your First name: ')
+lname = input('Enter your last name: ')
+name = fname + ' ' + lname
+client.execute("SELECT * FROM students WHERE name=%s", (name,))
+student = client.fetchone()
+if  student == None:
+      print("User doesn't exist \n")
+      add_new_user(name)
+else:
+      print(f"Name: {student[1]} \n ICT score: {student[2]} \n Math score: {student[3]} \n Chemistry score: {student[4]}")
 
-def find_user(users):
-      print('Find user')
-      fname = input('Enter your First name: ')
-      lname = input('Enter your last name: ')
-      name = fname + ' ' + lname
-      for user in users:
-            if user['name'] == name:
-                  print(f'user: {user["name"]} \n score: {user["score"]} \n percentage: {user["score"]}%')
-                  quit()
-      return True
 
-# add new user
 
-def add_new_user(users):
-      print('Add new user \n')
-      fname = input('Enter your First name: ')
-      lname = input('Enter your last name: ')
-      name = fname + ' ' + lname
-      score = input('Enter new score: ')
-      users += {'name': name, 'score': score}
-
-is_there = find_user(users)
-if is_there:
-      add_new_user(users)
